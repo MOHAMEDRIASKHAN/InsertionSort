69ec3514671da6071ec3e40b991e26a6ad7ad6e9
MOHAMEDRIASKHAN <108645671+MOHAMEDRIASKHAN@users.noreply.github.com>
2023-02-17 10:22:07 +0530

    Recheck the codes

--- Program.cs
+++ Program.cs
@@ -30,21 +30,7 @@ namespace InsertionSort
                 Console.WriteLine(ele);
             }
 
-            //string str1 = "heart";
-            //string str2 = "earth";
-
-            //char[] chr1 = str1.ToLower().ToCharArray();
-            //char[] chr2 = str2.ToLower().ToCharArray();
-
-            //Array.Sort(chr1);
-            //Array.Sort(chr2);
-
-            //string val1 = new string(chr1);
-            //string val2 = new string(chr2);
-
-            //Console.WriteLine(val1);
-            //Console.WriteLine(val2);    
-
+           
         }
     }
 }
